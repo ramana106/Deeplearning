2bd592221a02b169154fb8603a80701f878e4aa9
ram <ramanathan@madstreetden.com>
2017-10-27 08:38:19 +0530

    updated init info

--- model/alexnet.py
+++ model/alexnet.py
@@ -1,10 +1,11 @@
 import chainer.functions as F
 import chainer.links as L
+from chainer.links import caffe
 import chainer
 
 
 class MLP(chainer.Chain):
-    def __init__(self):
+    def __init__(self, n_out, init_net=False):
         super(MLP, self).__init__()
         with self.init_scope():
             self.conv1 = L.Convolution2D(None,  96, 11, stride=4)
@@ -14,7 +15,26 @@ class MLP(chainer.Chain):
             self.conv5 = L.Convolution2D(None, 256,  3, pad=1)
             self.fc6 = L.Linear(None, 4096)
             self.fc7 = L.Linear(None, 4096)
-            self.fc8 = L.Linear(None, 1000)
+            self.fc8 = L.Linear(None, init_net)
+
+        if init_net:
+            caffemodel = ''
+            func = caffe.CaffeFunction(caffemodel)
+            # Conv1
+            self.conv1.W.data = func.conv1.W.data
+            self.conv1.b.data = func.conv1.b.data
+            # Conv2
+            self.conv2.W.data = func.conv2.W.data
+            self.conv2.b.data = func.conv2.b.data
+            # Conv3
+            self.conv3.W.data = func.conv3.W.data
+            self.conv3.b.data = func.conv3.b.data
+            # Conv4
+            self.conv4.W.data = func.conv4.W.data
+            self.conv4.b.data = func.conv4.b.data
+            # Conv5
+            self.conv5.W.data = func.conv5.W.data
+            self.conv5.b.data = func.conv5.b.data
 
     def __call__(self, x, t):
         h = F.max_pooling_2d(F.local_response_normalization(
